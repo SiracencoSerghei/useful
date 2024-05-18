e9cfe4f7a367eff71e02889c08bfc89cb01f6ac4
Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>
2024-05-18 13:01:05 +0200

    docstring add for lint

--- git/hello.py
+++ git/hello.py
@@ -1,4 +1,7 @@
-
+"""
+    The `hello()` function in Python prints "Hello World!" to the console
+    """
+    
 def hello():
     """
     The `hello()` function in Python prints "Hello World!" to the console.
